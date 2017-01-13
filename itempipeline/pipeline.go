a38c8aaee43f074086476c7d371008d831981033
gaopeng527 <1259728004@qq.com>
2017-01-13 21:38:40 +0800

    条目处理管道

--- itempipeline/pipeline.go
+++ itempipeline/pipeline.go
@@ -1,6 +1,11 @@
 package itempipeline
 
-import "webcrawler/base"
+import (
+	"errors"
+	"fmt"
+	"sync/atomic"
+	"webcrawler/base"
+)
 
 // 条目处理管道的接口类型
 type ItemPipeline interface {
@@ -19,4 +24,83 @@ type ItemPipeline interface {
 	ProcessingNumber() uint64
 	// 获取摘要信息。
 	Summary() string
+}
+
+// 条目处理管道的实现类型
+type myItemPipeline struct {
+	itemProcessors   []ProcessItem // 条目处理器的列表
+	failFast         bool          // 表明处理是否需要快速失败的标志位
+	sent             uint64        // 已被发送的条目数量
+	accepted         uint64        // 已被接受的条目数量
+	processed        uint64        // 已被处理的条目数量
+	processingNumber uint64        // 正在被处理的条目数量
+}
+
+func NewItemPipeline(itemProcessors []ProcessItem) ItemPipeline {
+	if itemProcessors == nil {
+		panic(errors.New(fmt.Sprintln("Invalid item processor list!")))
+	}
+	innerItemProcessors := make([]ProcessItem, 0)
+	for _, ip := range itemProcessors {
+		if ip == nil {
+			panic(errors.New(fmt.Sprintf("Invalid item processor[%d]!\n", i)))
+		}
+		innerItemProcessors = append(innerItemProcessors, ip)
+	}
+	return &myItemPipeline{itemProcessors: innerItemProcessors}
+}
+
+func (ip *myItemPipeline) Send(item base.Item) []error {
+	atomic.AddUint64(&ip.processingNumber, 1)
+	defer atomic.AddUint64(&ip.processingNumber, ^uint64(0)) // 执行减一操作
+	atomic.AddUint64(&ip.sent, 1)
+	errs := make([]error, 0)
+	if item == nil {
+		errs = append(errs, errors.New("The item is invalid!\n"))
+		return errs
+	}
+	atomic.AddUint64(&ip.accepted, 1)
+	var currentItem base.Item = item
+	for _, itemProcessor := range ip.itemProcessors {
+		processedItem, err := itemProcessor(currentItem)
+		if err != nil {
+			errs = append(errs, err)
+			if ip.failFast {
+				break
+			}
+		}
+		if processedItem != nil {
+			currentItem = processedItem
+		}
+	}
+	atomic.AddUint64(&ip.processed, 1)
+	return errs
+}
+
+func (ip *myItemPipeline) FailFast() bool {
+	return ip.failFast
+}
+
+func (ip *myItemPipeline) SetFailFast(failFast bool) {
+	ip.failFast = failFast
+}
+
+func (ip *myItemPipeline) Count() []uint64 {
+	counts := make([]uint64, 3)
+	counts[0] = atomic.LoadUint64(&ip.sent)
+	counts[1] = atomic.LoadUint64(&ip.accepted)
+	counts[2] = atomic.LoadUint64(&ip.processed)
+	return counts
+}
+
+func (ip *myItemPipeline) ProcessingNumber() uint64 {
+	return atomic.LoadUint64(&ip.processingNumber)
+}
+
+var summaryTemplate = "failFast: %v, processorNumber: %d, sent: %d, accepted: %d, processed: %d, processingNumber: %d"
+
+func (ip *myItemPipeline) Summary() string {
+	counts := ip.Count()
+	summary := fmt.Sprintf(summaryTemplate, ip.failFast, len(ip.itemProcessors), counts[0], counts[1], counts[2], ip.ProcessingNumber())
+	return summary
 }
